09b4d4c097f09c8a95590b181da4c92c94953b2e
Roland <metro@live.hk>
2012-04-22 22:49:53 +0800

    add MDi support

--- VicisCycle/src/viciscycle/VicisCycle.java
+++ VicisCycle/src/viciscycle/VicisCycle.java
@@ -1,9 +1,8 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * VicisCycle 2012
+ * Released under GNU GPL License v2
  */
 package viciscycle;
-
 /**
  *
  * @author roland
@@ -14,6 +13,12 @@ public class VicisCycle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				new viciscycle.gui.VicisCycleGUI().setVisible(true);
+			}
+		});
     }
 }
--- VicisCycle/src/viciscycle/gui/VicisCycleGUI.java
+++ VicisCycle/src/viciscycle/gui/VicisCycleGUI.java
@@ -1,20 +1,65 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * VicisCycle 2012
+ * Released under GNU GPL License v2
  */
-package gui;
+/**TO-DOs
+ * 1. add background to main window
+ * 2. set internal window's size and layout
+ * 3. add international support 
+ */
+package viciscycle.gui;
+
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author roland
  */
-public class VicisCycleGUI extends javax.swing.JFrame {
+public class VicisCycleGUI extends javax.swing.JFrame 
+{
 
+	private JDesktopPane mainDesktopPane;
     /**
      * Creates new form VicisCycleGUI
      */
     public VicisCycleGUI() {
-        initComponents();
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setSize(800, 600);
+		setResizable(false);
+		mainDesktopPane = new JDesktopPane();
+		
+		add(mainDesktopPane);
+		//initComponents();
+		try 
+		{
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) 
+			{
+                if ("Nimbus".equals(info.getName())) 
+				{
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) 
+		{
+            java.util.logging.Logger.getLogger(VicisCycleGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) 
+		{
+            java.util.logging.Logger.getLogger(VicisCycleGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) 
+		{
+            java.util.logging.Logger.getLogger(VicisCycleGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) 
+		{
+            java.util.logging.Logger.getLogger(VicisCycleGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+		GameTitleFrame gameFrame = new GameTitleFrame();
+		mainDesktopPane.add(gameFrame,BorderLayout.CENTER);
+		gameFrame.setVisible(true);
     }
 
     /**
@@ -52,6 +97,27 @@ public class VicisCycleGUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+	class GameTitleFrame extends JInternalFrame
+	{
+		public GameTitleFrame()
+		{
+			super("VicisCycle - Title",false,false,false,true);
+			setBounds(0, 0, 320, 300);
+			setLocation((800-320)/2,(600-300)/2);
+			JPanel gp = new JPanel();
+			gp.setSize(320,300);
+			JButton b = new JButton("Start Server");
+			gp.add(b);
+			b = new JButton("Join Game");
+			gp.add(b);
+			b = new JButton("Credits");
+			gp.add(b);
+			b = new JButton("Exit");
+			gp.add(b);
+			add(gp, BorderLayout.CENTER);
+			pack();
+		}
+	}
     /**
      * @param args the command line arguments
      */
