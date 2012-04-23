71cbbc2b48e372dd8902ad70b71539d97b5e5594
Roland <metro@live.hk>
2012-04-24 00:21:20 +0800

    CHG: internationalization started, formatting,

--- VicisCycle/src/viciscycle/VicisCycle.java
+++ VicisCycle/src/viciscycle/VicisCycle.java
@@ -3,22 +3,22 @@
  * Released under GNU GPL License v2
  */
 package viciscycle;
+
 /**
  *
  * @author roland
  */
 public class VicisCycle {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable()
-		{
-			public void run()
-			{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
 				new viciscycle.gui.VicisCycleGUI().setVisible(true);
 			}
 		});
-    }
+	}
 }
