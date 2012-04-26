ab88ac70d8b1c195dad2222594ff8ce2ad0f8788
Roland <metro@live.hk>
2012-04-26 22:32:34 +0800

    CHG: game title page fixed

---
+++ VicisCycle/src/viciscycle/gui/GameCreditsInternalFrame.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viciscycle.gui;
+
+/**
+ *
+ * @author roland
+ */
+public class GameCreditsInternalFrame {
+	
+}
