091d0c7e0eed00abfcecb562ba44a7c6849254ae
Roland <metro@live.hk>
2012-05-07 23:11:05 +0800

    --2nd step to rendering tile,showing the whole tile on the screen--

--- VicisCycle/src/viciscycle/model/tile/Tile.java
+++ VicisCycle/src/viciscycle/model/tile/Tile.java
@@ -5,6 +5,7 @@
 package viciscycle.model.tile;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -21,8 +22,9 @@ public class Tile extends ImageIcon {
 		orientation = tileOrientation;
 		 Image image = Toolkit.getDefaultToolkit().getImage("src/viciscycle/graphics/"+TileSymbol.values()[  prototype.getTileSymbol().ordinal()]+".png");
 		this.setImage(image);
-		 shape2 = new Rectangle2D.Double(0, 0, 78, 98);
-		 shape1 = new Rectangle2D.Double(78, 0, 20, 98);
+		 shape2 = new Rectangle2D.Double(0, 0, 98, 78);
+		 shape1 = new Rectangle2D.Double(0, 78, 98, 20);
+		 border = new Rectangle2D.Double(0, 0, 95, 93);
 	}
 	
 	@Override
@@ -33,24 +35,36 @@ public class Tile extends ImageIcon {
 		//code to migrate
 		BufferedImage image = new BufferedImage(98, 98, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D gImage = image.createGraphics();
-		
+		gImage.rotate(-Math.PI/2);
+		gImage.translate(-98,0 );
 		//draw background
-		Point2D center =new Point2D.Float(48, 48);
+		/*Point2D center =new Point2D.Float(48, 48);
 		float radius = 76;
 		float dist[] = {0.4f,1.0f};
-		Color colors[] = {Color.RED,Color.WHITE};
-		gImage.setPaint(new RadialGradientPaint(center, radius, dist, colors));
+		Color colors[] = {Color.RED,Color.WHITE};*/
+		
+		gImage.setPaint(new GradientPaint(0,0,Color.WHITE, 34, 34, Color.RED));
 		gImage.fill(shape2);
-		Color colors2[] = {Color.ORANGE,Color.WHITE};
-		gImage.setPaint(new RadialGradientPaint(center, radius, dist, colors2));
+		//Color colors2[] = {Color.ORANGE,Color.WHITE};
+		gImage.setPaint(new GradientPaint(0,0,Color.WHITE, 34, 34, Color.ORANGE));
 		gImage.fill(shape1);
+		gImage.setStroke(new BasicStroke(5));
+		gImage.setPaint( Color.BLACK);
+		gImage.draw(border);
 		gImage.drawImage(this.getImage(),0,0,null);
-		AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.DST_ATOP, 1f);
-		gImage.setComposite(composite);
+	
 		
 		//draw shape
+		AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 0.3f);
+		gImage.setComposite(composite);
+		Ellipse2D e = new Ellipse2D.Double(-10, 39, 20, 20);
+		gImage.setStroke(new BasicStroke(2));
+		gImage.draw(e);
+		Rectangle2D r = new Rectangle2D.Double(88, 39, 20, 20);
+		gImage.draw(r);
 		
-		
+		 composite = AlphaComposite.getInstance(AlphaComposite.DST_ATOP, 1f);
+		gImage.setComposite(composite);
 		if (this.orientation == TileOrientation.SIDEWAYS){
 		g2d.rotate(Math.PI/2);
 		g2d.drawImage(this.getImage(), 0, -98, c);
@@ -71,4 +85,5 @@ public class Tile extends ImageIcon {
 	private final TileOrientation orientation;
 	private Shape shape2;
 	private Shape shape1;
+	private Shape border;
 }
